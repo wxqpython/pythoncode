df19ed6d5f6ee24ebeed3a200f6139d264ef395e
wxqpython <1312325639@qq.com>
2018-05-24 23:50:42 +0800

    Update download_rpm.py

--- download_rpm.py
+++ download_rpm.py
@@ -53,4 +53,35 @@ for i in tag_a:
     full_url = url + half_pk + "\n"
     download(full_url,half_pk)
     
+# 有一些想法，想对上面代码优化，然后代码就这样了
+import requests
+import re,os
+from bs4 import BeautifulSoup
+url = 'http://mirrors.163.com/centos/7.4.1708/os/x86_64/Packages/'
+
+def download(full_url,half_pk):
+    import requests
+    resource = requests.get(url=full_url, stream=True)
+    if os.path.exists(half_pk):
+        os.remove(half_pk)
+    with open(half_pk, mode="wb") as f:
+       for chunk in resource.iter_content(chunk_size=10240): # 10KB
+           f.write(chunk)
+    return None
+
+def getall_tag_a():
+    rep = re.compile('^\w')
+    r1 = requests.get(url=url,)
+    obj = BeautifulSoup(r1.text,'html.parser')
+    tag_a = obj.find_all(name='a',text=rep)
+    # return tag_a
+    for x in tag_a:
+        yield x
+
+for a in getall_tag_a():
+    half_pk = a.text
+    full_url = url + half_pk + "\n"
+    download(full_url,half_pk)
+    
+    
     
