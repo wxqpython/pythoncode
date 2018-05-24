f6bdc77cf8aa5d423a54f64be7a83e1938c8e038
wxqpython <1312325639@qq.com>
2018-05-25 00:55:28 +0800

    Update download_rpm.py

--- download_rpm.py
+++ download_rpm.py
@@ -19,7 +19,7 @@ tag_a = obj.find_all(name='a',text=rep)
 
 f = open(save_file,'a')
 for i in tag_a:
-    half_pk = i.text
+    half_pk = i.attrs.get('href')
     pk = url + half_pk + "\n"
     f.write(pk)
 f.close()
@@ -27,7 +27,7 @@ f.close()
 
 # 方法二：requests完全实现下载
 
-import requests
+import requests 
 import re,os
 from bs4 import BeautifulSoup
 
@@ -54,8 +54,8 @@ for i in tag_a:
     download(full_url,half_pk)
     
 # 有一些想法，想对上面代码优化，然后代码就这样了
-import requests
-import re,os
+import requests  # 代码未验证
+import re,os 
 from bs4 import BeautifulSoup
 url = 'http://mirrors.163.com/centos/7.4.1708/os/x86_64/Packages/'
 
@@ -80,6 +80,40 @@ def getall_tag_a():
 
 for a in getall_tag_a():
     half_pk = a.attrs.get('href')  # 取属性才对
+    full_url = url + half_pk
+    download(full_url,half_pk)
+    
+    
+    
+#!/usr/bin/env python3  # 代码经验证完好
+import threading
+import requests
+import re,os
+from bs4 import BeautifulSoup
+url = 'http://mirrors.163.com/centos/7.4.1708/os/x86_64/Packages/'
+
+def download(full_url,half_pk):
+    import requests
+    if os.path.exists(half_pk):
+        os.remove(half_pk)
+    resource = requests.get(url=full_url, )
+    resource.raise_for_status()
+    with open(half_pk, mode="wb") as f:
+           f.write(resource.content)
+    print('download ok %s' %  half_pk)
+    return None
+
+def getall_tag_a():
+    rep = re.compile('^\w')
+    r1 = requests.get(url=url,)
+    obj = BeautifulSoup(r1.text,'html.parser')
+    tag_a = obj.find_all(name='a',text=rep)
+#    return tag_a
+    for x in tag_a:
+        yield x
+
+for a in getall_tag_a():
+    half_pk = a.attrs.get('href')
     full_url = url + half_pk
     download(full_url,half_pk)
     
